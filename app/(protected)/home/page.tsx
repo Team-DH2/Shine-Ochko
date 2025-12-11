b5e7d9cb24bcf198600125e133ab943e2515bb4d
jasu55 <67340668+jasu55@users.noreply.github.com>
2025-12-11 16:53:33 +0800

    ankhaagiin ustgasang sergeew, map nemew

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -3,13 +3,13 @@
 import { CarouselMy } from "@/components/us/CarouselMy";
 import { Hero } from "@/components/us/Hero";
 import { WhyChooseUs } from "@/components/us/WhyChooseUs";
-import { LayoutFooter } from "@/components/us/LayoutFooter";
-
+import { Map } from "@/components/event-halls/map";
 export default function Page() {
   return (
     <main className="h-screen overflow-y-scroll snap-y snap-mandatory">
       <CarouselMy halls={[]} />
       <Hero />
+      <Map />
       <WhyChooseUs />
       <LayoutFooter />
     </main>
