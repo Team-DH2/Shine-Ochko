6dfc734ef4f2b35ed58fb3db132bc9c05e7e39b0
jasu55 <67340668+jasu55@users.noreply.github.com>
2025-12-11 17:37:08 +0800

    54 jamka (#60)
    * jamka nemelt main
    
    * ankhaagiin ustgasang sergeew, map nemew
    
    * suuld
    
    * map nemew

--- app/nasocomponent/EventHalls.tsx
+++ app/nasocomponent/EventHalls.tsx
@@ -147,7 +147,6 @@ export default function EventHalls() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {loading && <EventHallsSkeleton />}
-
             {!loading &&
               filteredHalls.map((hall) => (
                 <div
@@ -205,6 +204,7 @@ export default function EventHalls() {
                 Илэрц олдсонгүй.
               </div>
             )}
+            ``
           </div>
         </div>
       </div>
